9ae20ae098004a45d86e7c6c6452f76d02ed9136
พุฒิพงศ์ วราศรีทรัพย์ <kitsune@Kitsus-MacBook.local>
2023-11-23 07:38:52 +0700

    sent assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,16 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName="Karter";
+let lastName="Anderson";
+let address="54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+//
+const restaurantName="Chef's Table by Chef James";
+const netBill=15000;
+const avaliable=1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address="(970) 878-53292400 County Rd #4Meeker, ColoradoZ0X, 81641";
+netBill="5000";
+avaliable=2500;
+
--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,7 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
-
+console.log(celsius)
 // Start coding here;
+let fahrenheit = (celsius * 1.8) + 32;
+console.log(fahrenheit)
 
-let fahrenheit;
