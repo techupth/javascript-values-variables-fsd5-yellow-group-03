fb1f921a44d578aa6b90d9448e4e28c02326bf44
lacitadelle <jimmy.komala@outlook.com>
2023-11-23 13:57:48 +0700

    finished all 3 exercises

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,19 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+let billTotal = 15000;
+let operatingRestaurants = 1500;
+
+const restaurantName = "Chef's Table by Chef James";
+const billNetTotal = 15000;
+const restaurantCount = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+billTotal = 5000;
+operatingRestaurants = 2500;
